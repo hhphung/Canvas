7fe7bd2c7f306942098ed0468b7854c85d413f2f
akfriis <akfriis@iastate.edu>
2021-11-05 04:56:25 -0500

    skeleton structure combined calendar, home screen

--- home.js
+++ home.js
@@ -1,3 +1,8 @@
+function goToPage()
+{
+    location.href = "file:///C:/Users/amand/OneDrive/Desktop/COMS319-%20wrkspce/StudentPlanner/combinedCalendar.html";
+}
+
 function test()
 {
     alert("test");
