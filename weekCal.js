3d11c67f322986fc39702d200cdd13cb499df92f
akfriis <akfriis@iastate.edu>
2021-11-14 18:51:28 -0600

    Temporary enter event form. temporary event adding

--- weekCal.js
+++ weekCal.js
@@ -1,22 +1,42 @@
 /*calendar functions*/
+class Event 
+{
+    constructor(tit, colo) 
+    {
+        this.title = tit;
+        this.color = colo;
+    }
+}
+
+class Day 
+{
+    constructor(month, day, year) 
+    {
+        this.date = month +"/"+ day +"/"+ year;
+        this.events = [];
+    }
+
+    addEvent(event)
+    {
+        this.events.push(event);
+    }
+
+}
+
 today = new Date();
 currentMonth = today.getMonth();
 currentYear = today.getFullYear();
 currentWeek = (today.getDate() - today.getDay());
 
+var tempDayHolder = [new Day(currentMonth+1, today.getDate(), currentYear)];
 months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
-
-const day =
-{
-    events: [""],
-}
-
 monthAndYear = document.getElementById("monthAndYear");
 showWeek(currentWeek);
 
 var highlight;
 function highlightDay(ctrl)
 {  
+
     highlight.style.background = "rgb(7,0,50)";
     highlight.style.border = "0px";
     highlight = ctrl;
@@ -25,9 +45,7 @@ function highlightDay(ctrl)
     ctrl.style.border = "1px solid #FFFA9D"
     ctrl.style.borderRadius = "45px";
 
-    ctrl = day;
-    ctrl.events = ["clean", "zybooks"];
-    showEvents(ctrl);
+    showEvents(ctrl.innerHTML);
 }
 
 function onClick(){
@@ -124,7 +142,141 @@ function daysInMonth(iMonth, iYear) {
     return 32 - new Date(iYear, iMonth, 32).getDate();
 }
 
-function deleteEvent(toDelete)
+/*event stuff*/
+
+
+function deleteEvent(toDelete, dayIdx, eventIdx)
 {
     toDelete.remove();
+    //alert(tempDayHolder[dayIdx].events[eventIdx-1].title);
+    tempDayHolder[dayIdx].events.splice[eventIdx, 1];
 }
+
+function enter()
+{
+    var txt = document.getElementById("date").value;
+    var year = txt.substring(0,4);
+    var mon = txt.substring(5,7);
+    var day = txt.substring(8,10);
+    var color = document.getElementById("color").value;
+    var title = document.getElementById("title").value;
+
+    const dayIn = new Day(mon, day, year);
+    const eventIn = new Event(title, color);
+    var flag = -1;
+
+    if (tempDayHolder.length == 0)
+    {
+        dayIn.addEvent(eventIn);
+        tempDayHolder.push(dayIn);
+    }
+    else
+    {
+        for (var i = 0; i < tempDayHolder.length; ++i)
+        {
+            if (tempDayHolder[i].date == dayIn.date)
+            {
+                flag = i;
+                break;
+            }
+        }
+
+        if (flag == -1)
+        {
+            dayIn.addEvent(eventIn);
+            tempDayHolder.push(dayIn);
+        }
+        else
+        {
+            tempDayHolder[flag].addEvent(eventIn);
+            flag = -1;
+        }
+    }
+}
+
+
+function showEvents(day)
+{
+    let dateName = currentMonth+1 +"/"+ day +"/"+currentYear;
+    let newFlag = 0;
+
+    for (var h = 0; h < tempDayHolder.length; ++h)
+    {
+        if (dateName == tempDayHolder[h].date)
+        {
+            newFlag = h;
+        }
+    }
+
+    var events = document.getElementById("events-body");
+    events.innerHTML = "";
+    
+    if (tempDayHolder[newFlag].events.length == 0)
+    {
+        let row = document.createElement("tr");
+        let cell = document.createElement("td");
+        cell.setAttribute("class", "eventData");
+        cell.style.backgroundColor = "aliceblue";
+        cellText = document.createTextNode("No events for today!");
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+        events.appendChild(row);
+    }
+    else
+    {
+        for (var t = 0; t <= tempDayHolder[newFlag].events.length; ++t)
+        {
+            if (t == tempDayHolder[newFlag].events.length)
+            {
+                break;
+            }
+            let row = document.createElement("tr");
+            let cell = document.createElement("td");
+            cell.setAttribute("class", "eventData");
+            cell.style.backgroundColor = tempDayHolder[newFlag].events[t].color;
+            cellText = document.createTextNode(tempDayHolder[newFlag].events[t].title);
+            let xButton = document.createElement("button");
+            xButton.innerHTML = "X";
+            xButton.setAttribute("class", "xEvent");
+            xButton.onclick = function(){deleteEvent(cell, newFlag, t)};
+            cell.appendChild(cellText);
+            cell.appendChild(xButton);
+            row.appendChild(cell);
+            events.appendChild(row);
+        }
+        
+    }
+}
+
+
+
+
+    // let row = document.createElement("tr");
+    // let cell = document.createElement("td");
+    // cell.setAttribute("class", "eventData");
+    // cell.style.backgroundColor = day.color;
+    // cellText = document.createTextNode(day.events[i]);
+    // cell.appendChild(cellText);
+    // row.appendChild(cell);
+    // events.appendChild(row);
+
+/*
+11-16-2021
+    event1
+        Title
+        color
+    event2
+        title
+        color
+
+11-17-2021
+    event1
+        title
+        color
+    event2
+        title
+        color
+    event3
+        title
+        color
+*/
