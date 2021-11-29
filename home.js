bb13738171a932b1fc327d66ab46fe8e0f3b4e54
lith <litha@iastate.edu>
2021-11-29 15:45:27 -0600

    dynimic menu, fotter and transition

--- home.js
+++ home.js
@@ -1,9 +1,10 @@
-function goToPage()
-{
-    location.href = "file:///C:/Users/amand/OneDrive/Desktop/COMS319-%20wrkspce/StudentPlanner/combinedCalendar.html";
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+// Display mobile menu
+const mobileMenu = () =>{
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
 }
 
-function test()
-{
-    alert("test");
-}
+menu.addEventListener('click', mobileMenu);
