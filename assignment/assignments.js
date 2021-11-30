a85fc2606aa85f606e970e355be7e0fc7b90455b
lith <litha@iastate.edu>
2021-11-29 23:31:41 -0600

    transitions and new class schedule and some other fix

--- assignment/assignments.js
+++ assignment/assignments.js
@@ -1,25 +1,17 @@
-function test2()
-{
+function test2() {
     alert("test2");
 }
 
-function goHome()
-{
-    location.href = "file:///C:/Users/amand/OneDrive/Desktop/COMS319-%20wrkspce/StudentPlanner/home.html";
-}
+
 
 function addAssignment() {
- 
+
 }
 
-function highlight(ctrl)
-{
-    if (ctrl.style.background == 'rgb(255, 250, 157)')
-    {
+function highlight(ctrl) {
+    if (ctrl.style.background == 'rgb(255, 250, 157)') {
         ctrl.style.background = 'aliceblue';
-    }
-    else
-    {
+    } else {
         ctrl.style.background = 'rgb(255, 250, 157)';
     }
 }
@@ -57,9 +49,8 @@ function dates(n) {
         let day = weekday[dayind - 7];
     }
     var dd = today.getDate() + n;
-    var mm = month[today.getMonth()]; 
+    var mm = month[today.getMonth()];
 
-    
+
     return (day + ',' + mm + dd);
 }
-
---
+++ combinedCalendar/combinedCalendar.js
@@ -0,0 +1,13 @@
+function test2() {
+    alert("test2");
+}
+
+
+
+function highlight(ctrl) {
+    if (ctrl.style.background == 'rgb(255, 250, 157)') {
+        ctrl.style.background = 'aliceblue';
+    } else {
+        ctrl.style.background = 'rgb(255, 250, 157)';
+    }
+}
