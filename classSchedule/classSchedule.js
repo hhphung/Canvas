94366ba5d69a9f565c091ee46f48c73bff390236
lith <litha@iastate.edu>
2021-11-30 20:10:39 -0600

    speach mode

--- classSchedule/classSchedule.js
+++ classSchedule/classSchedule.js
@@ -1,5 +1,3 @@
-var a = document.getElementById("a");
-
 var b = document.getElementById("b");
 
 var c = document.getElementById("c");
@@ -8,22 +6,55 @@ var d = document.getElementById("d");
 
 var e = document.getElementById("e");
 
+var speak = document.getElementById("sm");
+
 var edit = document.getElementById("editB");
 
-b.addEventListener('click',classDetail);
-c.addEventListener('click',classDetail);
-d.addEventListener('click',classDetail);
-e.addEventListener('click',classDetail);
+b.addEventListener('click', classDetail);
+c.addEventListener('click', classDetail);
+d.addEventListener('click', classDetail);
+e.addEventListener('click', classDetail);
+speak.addEventListener('click', speakMode);
 
 edit.addEventListener("click", makevis);
 
 
 
 
-function classDetail(evt){
+function classDetail(evt) {
 
-    //alert("fuck")
-    alert(evt.currentTarget.innerHTML);
+    if (mode == 1) {
+
+        var msg = new SpeechSynthesisUtterance();
+        msg.text = evt.currentTarget.innerHTML;
+        window.speechSynthesis.speak(msg);
+
+
+    } else {
+
+        alert(evt.currentTarget.innerHTML);
+
+
+    }
+
+
+}
+
+var mode = 0;
+
+function speakMode() {
+    if (mode == 0) {
+
+        speak.innerHTML = "cancel";
+
+        mode = 1;
+    } else {
+
+        mode = 0;
+        speak.innerHTML = "speech mode";
+
+    }
+
 }
 
 
@@ -41,91 +72,155 @@ var iiii = document.getElementById("i4");
 
 var cn = document.getElementById("cancel");
 
-var cf =document.getElementById("confirm");
+var cf = document.getElementById("confirm");
 
 cn.addEventListener("click", canc);
 
-function makevis(){
+function makevis() {
+
+    if (mode == 1) {
+
+        var msg = new SpeechSynthesisUtterance();
+        msg.text = edit.innerHTML;
+        window.speechSynthesis.speak(msg);
 
 
-    i.style.display = 'block';
-    ii.style.display = 'block';
-    iii.style.display = 'block';
-    iiii.style.display = 'block';
-    cn.style.display = 'block';
-    cf.style.display = 'block';
-    
-    
+    } else {
+
+
+        i.style.display = 'block';
+        ii.style.display = 'block';
+        iii.style.display = 'block';
+        iiii.style.display = 'block';
+        cn.style.display = 'block';
+        cf.style.display = 'block';
+
+    }
+
+
 }
 
-function canc(){
+function canc() {
 
-    i.style.display = 'none';
-    ii.style.display = 'none';
-    iii.style.display = 'none';
-    iiii.style.display = 'none';
-    cn.style.display = 'none';
-    cf.style.display = 'none';
+    if (mode == 1) {
 
+        var msg = new SpeechSynthesisUtterance();
+        msg.text = cn.innerHTML;
+        window.speechSynthesis.speak(msg);
+
+
+    } else {
+
+        i.style.display = 'none';
+        ii.style.display = 'none';
+        iii.style.display = 'none';
+        iiii.style.display = 'none';
+        cn.style.display = 'none';
+        cf.style.display = 'none';
+    }
 
 }
 
 cf.addEventListener("click", conf);
-function conf(){
-    
-        // handle button click
-       
-            const rbs = document.querySelectorAll('input[name="choice"]');
-            let selectedValue;
-            for (const rb of rbs) {
-                if (rb.checked) {
-                    selectedValue = rb.value;
-                    break;
-                }
+
+function conf() {
+
+    // handle button click
+    if (mode == 1) {
+
+        var msg = new SpeechSynthesisUtterance();
+        msg.text = cf.innerHTML;
+        window.speechSynthesis.speak(msg);
+
+
+    } else {
+        const rbs = document.querySelectorAll('input[name="choice"]');
+        let selectedValue;
+        for (const rb of rbs) {
+            if (rb.checked) {
+                selectedValue = rb.value;
+                break;
             }
-            //alert(selectedValue);
-            var id;
+        }
+        //alert(selectedValue);
+        var id;
 
-            if(selectedValue == "class one"){
+        if (selectedValue == "class one") {
 
-                id = b;
+            id = b;
 
-            }
-            else if(selectedValue == "class two"){
+        } else if (selectedValue == "class two") {
 
-                id = c;
-
-
-
-
-            }
-            else if(selectedValue == "class three"){
-                
-                id = d;
+            id = c;
 
 
 
-            }
-            else if(selectedValue == "class four"){
 
-                id = e;
+        } else if (selectedValue == "class three") {
 
-
-            }
+            id = d;
 
 
-            var ans = prompt(selectedValue, "infomation");
 
-            id.innerHTML = ans;
+        } else if (selectedValue == "class four") {
+
+            id = e;
 
 
-            i.style.display = 'none';
-            ii.style.display = 'none';
-            iii.style.display = 'none';
-            iiii.style.display = 'none';
-            cn.style.display = 'none';
-            cf.style.display = 'none';
-    
+        }
+
+
+        var ans = prompt(selectedValue, "infomation");
+
+        id.innerHTML = ans;
+
+
+        i.style.display = 'none';
+        ii.style.display = 'none';
+        iii.style.display = 'none';
+        iiii.style.display = 'none';
+        cn.style.display = 'none';
+        cf.style.display = 'none';
+    }
+
 
 
 }
+
+function speaker(evt) {
+
+    if (mode == 1) {
+
+        var msg = new SpeechSynthesisUtterance();
+        msg.text = evt.currentTarget.innerHTML;
+        window.speechSynthesis.speak(msg);
+
+
+    }
+
+
+
+}
+
+
+
+
+
+const elements = document.querySelectorAll('.day');
+
+// adding the event listener by looping
+elements.forEach(element => {
+    element.addEventListener("click", function(evt) {
+        //this function does stuff
+        if (mode == 1) {
+
+
+
+            var msg = new SpeechSynthesisUtterance();
+            msg.text = evt.currentTarget.innerHTML;
+            window.speechSynthesis.speak(msg);
+
+
+        }
+    });
+});
